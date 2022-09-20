cb4cba42c8448e679d678e6d58786b999e2fc05b
root <root@DESKTOP-2HRIDSH.localdomain>
2022-09-19 21:40:48 -0400

    Add /time path, returns current time. Update docker file

--- time_app/run.py
+++ time_app/run.py
@@ -1,3 +1,6 @@
+# Thomas Wong, tsw8626
+from datetime import datetime
+import pytz
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,6 +9,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
+# Adding code for the new "/time" path here. When the server is running, visiting
+# "127.0.0.1:8000/time" returns the current time (NY time zone)
+@app.route("/time")
+def get_time():
+    nyTimeZone = pytz.timezone("America/New_York")
+    return datetime.now(nyTimeZone).strftime("%H:%M:%S")
 
 app.run(host='0.0.0.0',
         port=8080,
